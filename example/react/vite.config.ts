d0590ee39e771ec5bb209ae90264b82218a65276
Aroldo <aroldogooulart@gmail.com>
2024-02-29 23:42:15 -0400

    Add Vite configuration and React files

---
+++ example/react/vite.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  port: 5151, // This is the port which we will use in docker
+})
